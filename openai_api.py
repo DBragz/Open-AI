fdfa4a6b5b7f970086b5e51d7b10f0d2797a7daa
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2023-03-08 19:50:12 -0500

    Adding open ai api

---
+++ openai_api.py
@@ -0,0 +1,21 @@
+import openai
+
+# Insert your own OpenAI API key here
+openai.api_key = ""
+
+# Prompt the user to enter a question
+question = input("What is your question for OpenAI?")
+
+# Send the question to OpenAI and get the response
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=question,
+    max_tokens=2048
+)
+
+# Save the response to a text file
+with open("openai_response.txt", "w") as f:
+    f.write(response["choices"][0]["text"])
+
+print("The OpenAI response has been saved to openai_response.txt.")
+
