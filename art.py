41d1c63d8bec18d9f809b501ded8d9854ba645b9
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2024-01-09 00:33:21 -0500

    Added an art generator and more files to requirements.

--- art.py
+++ art.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 # Create a pixel image
 image = np.zeros((52, 85, 3), dtype=np.uint8)
 
-# Draw a red circle
-image[10:50, 10:50, 0] = 255
+# Draw a cool pixel gradient
+for i in range(52):
+    for j in range(85):
+        image[i, j] = [i * 5, j * 3, (i + j) * 3]
+
+# Add some text in a cool font and color
+plt.text(40, 20, "Hello", fontsize=20, fontweight='bold', color='magenta')
+plt.text(40, 25, "Oi", fontsize=20, fontweight='bold', color='cyan')
 
 # Display the image
 plt.imshow(image)
